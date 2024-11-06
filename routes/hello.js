80c1e57e3ca762a750928323adcf3a5401356fa6
Hikari567 <m9usoed3sh0mari@gmail.com>
2024-11-06 20:43:55 +0900

    Update hello.js

--- routes/hello.js
+++ routes/hello.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(reg, res, next){
-  res.render('hello', { title: 'Hello Express' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hello Express' });
 });
 
 module.exports = router;
