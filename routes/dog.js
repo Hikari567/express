05ae2781b7e710454a963114dce02d7b0cd0099b
Hikari567 <m9usoed3sh0mari@gmail.com>
2024-12-12 12:12:34 +0900

    add helo page

--- routes/dog.js
+++ routes/dog.js
@@ -1,8 +1,18 @@
-async function displayRandomDog(){       
-    const url = "https://dog.ceo/api/breeds/image/random";
-    const response = await fetch(url)
-    data = await response.json();       
-    const object = document.getElementById("randomDog");
-    object.innerHTML = `<img src="${data.message}">`;
-}
-displayRandomDog();
+const button = document.getElementById('click');
+    const type = document.getElementById('type');
+    const image = document.getElementById('image');
+    // ボタンをクリックしたらイベント発動
+    button.addEventListener('click', () => {
+        fetch('https://dog.ceo/api/breeds/image/random') // APIのURL
+        .then(response => {
+            return response.json();
+        })
+        .then(myJson => {
+            // imgタグに取得したURL画像を入れる
+            image.src = myJson['message'];
+            // 取得したURLをスラッシュ記号で分割し、犬種を表した要素を取得する
+            const breed = myJson['message'].split('/')[4];
+            type.innerText = breed;
+            button.innerText = 'わんわんお！'
+        });
+    });
