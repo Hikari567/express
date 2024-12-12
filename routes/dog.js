50990bde9ae0b4c25faa732617aba90122ad88d1
Hikari567 <m9usoed3sh0mari@gmail.com>
2024-12-12 16:31:26 +0900

    add helo page

--- routes/dog.js
+++ routes/dog.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 router.get('/', async (req, res) => {
-request('https://dog.ceo/api/breed//images/random', function (error, response,
+request.get('https://dog.ceo/api/breeds/image/random', function (error, response,
 body) {
-if (!error && response.statusCode == success) {
+if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
 res.json(data);
 }
